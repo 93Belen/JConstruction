d4066a72291a347042cb5f6f282a446f48a70743
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-08-17 09:41:30 -0500

    getting objects ready for jaimes texts

---
+++ content/text/about.ts
@@ -0,0 +1,33 @@
+const spanish = {
+    woweare: '',
+    ourprocess: '',
+    whatwedo: '',
+    process: {
+        a: '',
+        b: "",
+        c: "",
+        d: "",
+        e: ""
+    },
+    contacttext: ''
+}
+
+ const english = {
+    woweare: '',
+    ourprocess: '',
+    whatwedo: '',
+    process: {
+        a: '',
+        b: "",
+        c: "",
+        d: "",
+        e: ""
+    },
+    contacttext: ''
+}
+
+export const selectedLanguage = {
+    spanish,
+    english
+}
+
---
+++ content/text/services.ts
@@ -0,0 +1,25 @@
+const spanish = {
+    services: {
+        a: '',
+        b: "",
+        c: "",
+        d: ""
+    },
+    contacttext: ''
+}
+
+ const english = {
+    services: {
+        a: '',
+        b: "",
+        c: "",
+        d: ""
+    },
+    contacttext: ''
+}
+
+export const selectedLanguage = {
+    spanish,
+    english
+}
+
