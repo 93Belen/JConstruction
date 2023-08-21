df7042740669011a530bdc4a316343fb6e091e7f
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-08-21 14:01:41 -0500

    links instead buttons

--- app/components/ContactButtonOrange.tsx
+++ app/components/ContactButtonOrange.tsx
@@ -2,6 +2,7 @@
 
 import { useWebStore } from "../state/store";
 import {selectedLanguage} from "components/content/contanbutton";
+import Link from "next/link";
 
 export default function ContactButtonOrange(){
 
@@ -11,8 +12,8 @@ export default function ContactButtonOrange(){
 
 
     return (
-        <button className='text-white min-w-fit bg-orange rounded-md py-[0.625rem] px-[2.5rem] text-[1.25rem] w-[10rem]'>
+        <Link href='/contact' className='text-white no-underline min-w-fit bg-orange rounded-md py-[0.625rem] px-[2.5rem] text-[1.25rem] w-[10rem]'>
             {content}
-        </button>
+        </Link>
     )
 }
