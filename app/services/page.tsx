b7bd5dc1aa13771447bd5fd50a65985d7654066c
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-08-27 11:41:00 -0500

    icons

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -4,8 +4,8 @@ import { anton } from "../layout";
 import { useWebStore } from "../state/store";
 import { selectedLanguage } from "components/content/headers";
 import { selectedLanguage as textcontent } from "components/content/text/services";
-import { MdOutlineKitchen } from 'react-icons/md'
-import { FaShower } from 'react-icons/fa'
+import { PiTree } from 'react-icons/pi'
+import { MdFence } from 'react-icons/md'
 import { BsHouseDoor } from 'react-icons/bs'
 import { GiConcreteBag } from 'react-icons/gi'
 import ContactButtonOrange from "../components/ContactButtonOrange";
@@ -26,28 +26,28 @@ export default function Services(){
                 <div className='flex flex-col md:grid md:grid-cols-2 lg:flex lg:flex-row justify-center xl:w-[80%] xl:gap-10 gap-6 max-w-[1500px] m-auto md:py-14'>
                         <div className='px-4 py-3 flex flex-col gap-2 text-white lg:w-[300px]'>
                             <div className='p-2 border-white border-1 rounded-sm w-fit'>
-                                <MdOutlineKitchen className='text-[50px] text-orange'/>
+                                <BsHouseDoor className='text-[50px] text-orange'/>
                             </div>
                         <h4 className='font-bold text-[1.5rem]'>{content.services.a.name}</h4>
                         <p>{content.services.a.p}</p>
                     </div>
                     <div className='px-4 py-3 flex flex-col gap-2 text-white lg:w-[300px]'>
                             <div className='p-2 border-white border-1 rounded-sm w-fit'>
-                                <FaShower className='text-[50px] text-orange'/>
+                                <GiConcreteBag className='text-[50px] text-orange'/>
                             </div>
                         <h4 className='font-bold text-[1.5rem]'>{content.services.b.name}</h4>
                         <p className='text-[1rem]'>{content.services.b.p}</p>
                     </div>
                     <div className='px-4 py-3 flex flex-col gap-2 text-white lg:w-[300px]'>
                             <div className='p-2 border-white border-1 rounded-sm w-fit'>
-                                <BsHouseDoor className='text-[50px] text-orange'/>
+                                <MdFence className='text-[50px] text-orange'/>
                             </div>
                         <h4 className='font-bold text-[1.5rem]'>{content.services.c.name}</h4>
                         <p className='text-[1rem]'>{content.services.c.p}</p>
                     </div>
                     <div className='px-4 py-3 flex flex-col gap-2 text-white lg:w-[300px]'>
                             <div className='p-2 border-white border-1 rounded-sm w-fit'>
-                                <GiConcreteBag className='text-[50px] text-orange'/>
+                                <PiTree className='text-[50px] text-orange'/>
                             </div>
                         <h4 className='font-bold text-[1.5rem]'>{content.services.d.name}</h4>
                         <p className='text-[1rem]'>{content.services.d.p}</p>
